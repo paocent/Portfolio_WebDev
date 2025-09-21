83eea769e8a554f88f8c8be969cc42ab13c6a01d
Paolo Adame <196263400+paocent@users.noreply.github.com>
2025-09-20 22:07:32 -0400

    Update vite.config.js

--- 1._Portfolio/My_Personal_Porfolio/client/vite.config.js
+++ 1._Portfolio/My_Personal_Porfolio/client/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '//Portfolio_WebDev/1._Portfolio/My_Personal_Porfolio/client/',
+  base: process.env.VITE_BASE_PATH || '//Portfolio_WebDev',
 })
